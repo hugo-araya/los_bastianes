955269d1ca41ce513807d5d9f20aa231b6c500d6
Hugo Araya C. <42989121+hugo-araya@users.noreply.github.com>
2024-05-13 12:43:26 -0400

    hhh

---
+++ fun1.py
@@ -0,0 +1,21 @@
+import mibib as yo
+
+def fun1(x):
+    resultado = 2 * x + 1
+    return resultado
+
+def fun2(x):
+    resultado = 2 * x + 1
+    print(resultado)
+
+def fun3():
+    print('Hola mundo')
+
+if __name__ == '__main__':
+    print(__name__)
+    yo.saludar()
+    fun3()
+
+
+
+
